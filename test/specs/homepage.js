f07e33c53b3a031eeec33f63f0c73059bb32bdeb
Arthur Senko <arthursenko@gmail.com>
2019-08-06 21:26:00 -0700

    new str

--- test/specs/homepage.js
+++ test/specs/homepage.js
@@ -1,21 +1,18 @@
 import { assert } from 'chai';
+import exp from '../expected/homepage.exp';
+import sel from '../selectors/homepage.sel';
 
 describe('Client', function () { //define suite title by passing a string
 
   it('Page title', function () { //define test title by passing a string
     browser.url('/'); //open baseUrl + string passed in .url() function
     let title = browser.getTitle(); //get page title and assign it to the "title" variable
-    assert.equal(title, 'Progress Monitor'); //compare {title} (actual) and "Bug Tracker" (expected)
+    assert.equal(title, exp.pageTitle); //compare {title} (actual) and "Bug Tracker" (expected)
   })
 
   it('Title and version', function () {
-    let title = $('#site-name').getText();
-    assert.equal(title, 'Progress Monitor 0.1.61');
-  })
-
-  it('Title and version', function () {
-    let title = $('#site-name').getText();
-    assert.equal(title, 'Progress Monitor 0.1.61');
+    let title = $(sel.siteName).getText();
+    assert.equal(title, exp.titleVersion);
   })
 
 });
