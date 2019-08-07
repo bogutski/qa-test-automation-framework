8f443507fcabca5415471733eb8002414f698e02
Arthur Senko <arthursenko@gmail.com>
2019-08-06 20:22:50 -0700

    initial coomit

---
+++ test/test.js
@@ -0,0 +1,11 @@
+import { assert } from 'chai';
+
+describe('Client', function () { //define suite title by passing a string
+
+    it('Get title', function () { //define test title by passing a string
+      browser.url('/'); //open baseUrl + string passed in .url() function
+      let title = browser.getTitle(); //get page title and assign it to the "title" variable
+      assert.equal(title, 'Progress Monitor'); //compare {title} (actual) and "Bug Tracker" (expected)
+    })
+
+});
