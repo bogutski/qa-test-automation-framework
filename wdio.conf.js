a31d00ffa28282c4e56d241391f382a70d6c3e08
Arthur Senko <arthursenko@gmail.com>
2019-08-06 21:20:18 -0700

    folder str

--- wdio.conf.js
+++ wdio.conf.js
@@ -17,7 +17,7 @@ exports.config = {
     // directory is where your package.json resides, so `wdio` will be called from there.
     //
     specs: [
-        './test/*.js'
+        './test/specs/*.js'
     ],
     // Patterns to exclude.
     exclude: [
